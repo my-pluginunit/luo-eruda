f0423bb986bf140259cf4b1f67be7bed3b8d0695
罗利华 <luolihua@luolihuadeMacBook-Pro.local>
2021-09-27 14:32:22 +0800

    代码还原

--- index.js
+++ index.js
@@ -1,105 +1,31 @@
 "use strict";
 !function (e, f) {
-  typeof exports === "object" && typeof module === "object" ? module.exports = f() : typeof define === "function" && define.amd ? define([], f) : typeof exports === "object" ? exports.luoInfiniteScroll = f() : e.luoInfiniteScroll = f();
+  typeof exports === "object" && typeof module === "object" ? module.exports = f() : typeof define === "function" && define.amd ? define([], f) : typeof exports === "object" ? exports.luoEruda = f() : e.luoEruda = f();
 }(this, function () {
-  var Vue = require('vue');
-  var $ = require('jquery');
-
-  var ctx = '@@LuoInfiniteScroll';
-
-  var isAttached = function (element) {
-    var currentNode = element.parentNode;
-    while (currentNode) {
-      if (currentNode.tagName === 'HTML') return true;
-      if (currentNode.nodeType === 11) return false;
-      currentNode = currentNode.parentNode;
+  return function (va, vb) {
+    if (va) {
+      var reg = new RegExp("^((localhost)|(\\d+\\.\\d+\\.\\d+\\.\\d+))");
+      if (reg.test(window.location.host)) return insertScript(function () {eruda.init();});
+      
+      if (typeof va === "string" && window.location.host === va) return insertScript(function () {eruda.init();});
+      if ((va instanceof Array) && (va.indexOf(window.location.host) > -1)) return insertScript(function () {eruda.init();});
+      if ((va instanceof Object) && va.exclude) {
+        if (!(va.exclude instanceof Array)) throw new Error("参数类型有误！");
+        if (va.exclude.indexOf(window.location.host) > -1) return;
+        insertScript(function () {eruda.init();});
+      }
+    } else {
+      insertScript(function () {eruda.init();});
     }
-    return false;
-  };
-
-  var getScrollTop = function (element) {
-    if (element === window) return Math.max(window.pageYOffset || 0, document.documentElement.scrollTop);
-    return element.scrollTop;
-  };
-  
-  var getVisibleHeight = function (element) {
-    if (element === window) return document.documentElement.clientHeight;
-    return element.clientHeight;
-  };
-
-  var doBind = function (e) {
-    var directive = this;
-    var element = directive.el;
-    directive.scrollEventTarget = element;
-    directive.scrollListener = doCheck.bind(directive);
-    $(directive.scrollEventTarget).off('mousewheel').on('mousewheel', directive.scrollListener);
-  };
-
-  var doCheck = function  (e) {
-    var directive = this;
-    var viewportScrollTop = getScrollTop(directive.el);
-    var viewportBottom = viewportScrollTop + getVisibleHeight(directive.el);
-    var distanceExpr = directive.el.getAttribute('luo-infinite-scroll-distance');
-    var distance = 0;
-    if (distanceExpr) {
-      distance = Number(directive.vm[distanceExpr] || distanceExpr);
-      if (isNaN(distance)) distance = 0;
+    function insertScript (callback) {
+      if (vb && (!isMobile())) return;
+      var script = document.createElement("script");
+      script.src = "//cdn.jsdelivr.net/npm/eruda";
+      window.document.head.appendChild(script);
+      script.addEventListener("load", callback);
     }
-    directive.distance = distance;
-    var img = directive.el.getAttribute('luo-infinite-scroll-loding-path');
-  
-    var shouldTrigger = directive.el.scrollHeight - viewportBottom <= distance;
-  
-    if (e.originalEvent.deltaY > 0 && shouldTrigger && directive.expression) {
-      $('#__luo-infinite-scroll-loding__').remove();
-      $(directive.el).append('<div id="__luo-infinite-scroll-loding__" style="display: flex;flex-direction: row;align-items: center;justify-content: center;"><img width="100px" src="' + img + '" /></div>');
-      if (directive.v) clearTimeout(directive.v);
-      directive.v = setTimeout(function () {directive.t = false; $('#__luo-infinite-scroll-loding__').remove();}, 100);
-      if (directive.t) return;
-      directive.t = true;
-      directive.expression();
-    }
-  };
-
-  var LuoInfiniteScroll = {
-    bind: function bind (el, binding, vnode) {
-      el[ctx] = {
-        el: el,
-        vm: vnode.context,
-        expression: binding.value,
-        t: false,
-        v: null
-      };
-      var args = arguments;
-      el[ctx].vm.$on('hook:mounted', function () {
-        el[ctx].vm.$nextTick(function () {
-          if (isAttached(el)) return doBind.call(el[ctx], args);
-
-          el[ctx].bindTryCount = 0;
-          var tryBind = function tryBind() {
-            if (el[ctx].bindTryCount > 10) return;
-            el[ctx].bindTryCount++;
-            if (isAttached(el)) {
-              doBind.call(el[ctx], args);
-            } else {
-              setTimeout(tryBind, 50);
-            }
-          };
-          tryBind();
-        });
-      });
-    },
-    unbind: function unbind(el) {
-      if (el && el[ctx] && el[ctx].scrollEventTarget) $(el[ctx].scrollEventTarget).off('mousewheel');
-    }
-  };
-
-  var install = function install(Vue) {Vue.directive('LuoInfiniteScroll', LuoInfiniteScroll);};
-
-  window.LuoInfiniteScroll = LuoInfiniteScroll;
-  Vue.use(install);
-
-  LuoInfiniteScroll.install = install;
-
-  return LuoInfiniteScroll;
+    function isMobile () {
+      return !!window.navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i);
+    };
+  }
 });
